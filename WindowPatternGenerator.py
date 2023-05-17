c1c3fbcc02fab3486d6a102986a343786ba52feb
Michał <rusinekek@gmail.com>
2023-05-17 16:01:18 +0200

    Added MFII pattern and added reading from images

--- WindowPatternGenerator.py
+++ WindowPatternGenerator.py
@@ -6,19 +6,48 @@ import random
 # licze od lewego gornego okna
 
 class WindowPatternGenerator:
+  # musza byc wymiary 28x5
+  def arr_from_image(self,name,emulate = False):
+    pixel_arr = np.array(Image.open(name))
+    # naprawa png
+    if(np.shape(pixel_arr)[2] == 4):
+      new_pixel_arr = []
+      for i in range(np.shape(pixel_arr)[0]):
+        for j in range(np.shape(pixel_arr)[1]):
+          new_pixel_arr.append(pixel_arr[i][j][:3])
+      pixel_arr = np.reshape(new_pixel_arr,(5,28,3))
+      
+    for i in range(np.shape(pixel_arr)[0]):
+      for j in range(np.shape(pixel_arr)[1]):
+        tmp = pixel_arr[i][j][1]
+        pixel_arr[i][j][1] = pixel_arr[i][j][2]
+        pixel_arr[i][j][2] = tmp
+    return pixel_arr.flatten()[11*3:] if not emulate else self.emulate(pixel_arr)
+
   def poland_flag_arr(self,top_height,emulate = False):
-    poland = [[255,255,255],[220,20,60]]
+    # R B G
+    poland = [[255,255,255],[220,60,20]]
     out = self.make_flag(poland,top_height)
     return out.flatten()[11*3:] if not emulate else self.emulate(out)
 
   def ukraine_flag_arr(self,top_height,emulate = False):
-    ukraine = [[0,87,183],[255,221,0]]
+    ukraine = [[0,183,87],[255,0,221]]
     out = self.make_flag(ukraine,top_height)
     return out.flatten()[11*3:] if not emulate else self.emulate(out)
   
   def rainbow_arr(self,emulate = False):
     out = self.make_random_colors()
     return out.flatten()[11*3:] if not emulate else self.emulate(out)
+  
+  # returns an array of arrays ready to load with different frames
+  def animated_rainbow_arr(self,frames=10):
+    out = []
+    for _ in range(frames):
+      out.append(self.make_random_colors().flatten()[11*3:])
+    return out
+
+  def animated_flag(self):
+    pass
     
   def make_flag(self,country,top_height):
     rgb_arr = []
@@ -39,13 +68,13 @@ class WindowPatternGenerator:
     # values for non-existant windows
     for i in range(fifth_floor_non_exsitant_windows):
       row.append(0) # R
-      row.append(0) # G
       row.append(0) # B
+      row.append(0) # G
 
     for i in range(fifth_floor_remaining_windows):
       row.append(top_R_color) # R
-      row.append(top_G_color) # G
-      row.append(top_B_color) # B
+      row.append(top_G_color) # B
+      row.append(top_B_color) # G
 
     rgb_arr.append(row)
     row = []
@@ -56,8 +85,8 @@ class WindowPatternGenerator:
       # j - kolumny
       for _ in range(normal_floor_window_count):
         row.append(top_R_color) # R
-        row.append(top_G_color) # G
-        row.append(top_B_color) # B
+        row.append(top_G_color) # B
+        row.append(top_B_color) # G
       rgb_arr.append(row)
       row = []
 
@@ -92,14 +121,19 @@ class WindowPatternGenerator:
     for _ in range(4):
       for _ in range(normal_floor_window_count):
         row.append(random.randint(0,255)) # R
-        row.append(random.randint(0,255)) # G
         row.append(random.randint(0,255)) # B
+        row.append(random.randint(0,255)) # G
       rgb_arr.append(row)
       row = []
 
     return np.array(rgb_arr)
 
   def emulate(self,out):
+    for i in range(0,np.shape(out)[0]):
+      for j in range(0,np.shape(out)[1]):
+        tmp = out[i][j][1]
+        out[i][j][1] = out[i][j][2]
+        out[i][j][2] = tmp
     out_mod = out
     image_arr = out_mod.reshape(5,28,3).astype(np.uint8)
     windows = Image.fromarray(image_arr)
@@ -111,6 +145,6 @@ win = WindowPatternGenerator()
 
 # win.poland_flag_arr(2,emulate=True)
 # win.ukraine_flag_arr(2,emulate=True)
-print(win.rainbow_arr())
-
-# print(win.poland_flag_arr(2))
+# win.rainbow_arr(emulate=True)
+# win.arr_from_image("MFII.jpg", emulate=True)
+# print(win.animated_rainbow_arr(2))
